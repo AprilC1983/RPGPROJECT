54aac8db286cb481127a6aaa640701ed2640e035
AprilC1983 <friday_447@hotmail.com>
2015-12-08 01:40:10 -0500

    Create Dragon.py
    Still needs some work

---
+++ Dragon.py
@@ -0,0 +1,13 @@
+# Name:		Dragon
+# Purpose:	Class for creating dragon
+# Date:		November 30, 2015
+# Authors:	Robert Monsen, April May
+
+class Dragon:
+	def __init__(self):
+		self.hp = 37
+		self.atk = 5
+	def getATK(self):
+		return self.atk
+	def getHP(self):
+		return self.hp
