a60f7ac44bb56766ca1fae9e9668bdc922dbc1d6
AprilC1983 <friday_447@hotmail.com>
2015-12-08 22:35:43 -0500

    Update SelectCharacter.py

--- SelectCharacter.py
+++ SelectCharacter.py
@@ -1,14 +1,18 @@
 # Name:		SelectCharacter
 # Purpose:	Allows user to chooose what type of character to play
 # Date:		November 25, 2015
-# Authors:	Robert Monsen, April May
+# Authors:	April May
+
+from tkinter import *
 		
-class SelectCharacter():
+class SelectCharacter(object):
 	def __init__(self):
 		window = Tk()
 		window.title("Choose Your Character")
 		
-		type = ""
+		self.__type = "Great Void"
+		self.__hp = 5
+		self.__atk = 999
 		frame1 = Frame(window)
 		frame1.pack()
 		self.v1 = StringVar()
@@ -17,10 +21,14 @@ class SelectCharacter():
 		rbKnight = Radiobutton(frame1, text = "Knight", variable = self.v1, value = '3', command = self.processRadiobutton)
 		rbDwarf = Radiobutton(frame1, text = "Dwarf", variable = self.v1, value = '4', command = self.processRadiobutton)
 		rbFairy = Radiobutton(frame1, text = "Fairy", variable = self.v1, value = '5', command = self.processRadiobutton)
+		rbTheVoid = Radiobutton(frame1, text = "Nothing", variable = self.v1, value = '6', command = self.processRadiobutton)
+		self.v1.set('6')
 		
 		frame2 = Frame(window)
 		frame2.pack()
 		self.v2 = 3
+		self.lbl = Label(frame2, text = "Choose you character type. When you have made your selection click the 'OK' button.")
+		self.lbl.grid(row = 1, column = self.v2)
 		
 		frame3 = Frame(window)
 		frame3.pack()
@@ -36,17 +44,34 @@ class SelectCharacter():
 		
 		window.mainloop()
 		
-		def processRadiobutton(self):
-			if self.v1.get() == '1':
-				self.type = "Wizard"
-			elif self.v1.get() == '2':
-				self.type = "Elf"
-			elif self.v1.get() == '3':
-				self.type = "Knight"
-			elif self.v1.get() == '4':
-				self.type = "Dwarf"
-			elif self.v1.get() == '5':
-				self.type = "Fairy"
+	def processRadiobutton(self):
+		if self.v1.get() == '1':
+			self.__type = "Wizard"
+			self.__hp = 10
+			self.__atk = 8
+		elif self.v1.get() == '2':
+			self.__type = "Elf"
+			self.__hp = 8
+			self.__atk = 12
+		elif self.v1.get() == '3':
+			self.__type = "Knight"
+			self.__hp = 9
+			self.__atk = 12
+		elif self.v1.get() == '4':
+			self.__type = "Dwarf"
+			self.__hp = 15
+			self.__atk = 11
+		elif self.v1.get() == '5':
+			self.__type = "Fairy"
+			self.__hp = 20
+			self.__atk = 3
+		elif self.v1.get() == '6':
+			self.__type = "The Void"
+			self.__hp = 0
 	
-		def getType(self):
-			return self.type
+	def getType(self):
+		return self.__type
+	def getHP(self):
+		return self.__hp
+	def getATK(self):
+		return self.__atk
