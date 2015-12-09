e174de985bef2277a968bf92c6b827ecf7e436f3
AprilC1983 <friday_447@hotmail.com>
2015-12-08 22:38:41 -0500

    Update Dragon.py
    Some new methods added

--- Dragon.py
+++ Dragon.py
@@ -1,13 +1,24 @@
 # Name:		Dragon
 # Purpose:	Class for creating dragon
 # Date:		November 30, 2015
-# Authors:	Robert Monsen, April May
+# Authors:	April May
 
 class Dragon:
 	def __init__(self):
-		self.hp = 37
-		self.atk = 5
+		self.hp = 5
+		self.atk = 20
+		self.x = 350
+		self.y = 175
+		self.dx = 2 #move right by default
+		self.radius = 5
+		self.color = "red"
+		
+	def reduceATK(self):
+		self.atk -= 5
+	def setHP(self, hit):
+		self.hp += hit
 	def getATK(self):
 		return self.atk
 	def getHP(self):
 		return self.hp
+	
